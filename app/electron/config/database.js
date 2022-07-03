9e9324a83824b5595599aff2e7a16d20dd92ac2e
fenil jariwala <feniljariwala82@gmail.com>
2022-07-03 14:08:58 +0530

    pushing encrypted db

--- app/electron/config/database.js
+++ app/electron/config/database.js
@@ -3,13 +3,19 @@ const path = require('path');
 
 const myDBConfig = {
   client: 'sqlite3',
-  connection: () => ({
+  connection: {
     filename: process.env.DATABASE_PATH,
-  }),
+  },
   migrations: {
     directory: path.join(__dirname, '..', 'migrations'),
   },
   useNullAsDefault: true,
+  pool: {
+    afterCreate: function (conn, done) {
+      conn.run("PRAGMA KEY = 'password'");
+      done();
+    },
+  },
 };
 
 const knex = require('knex')(myDBConfig);
--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,61 @@
-const path = require("path");
+const path = require('path');
 
 // Update with your config settings.
+
+console.log(path.join(__dirname, 'databases', 'todo_hut_app.sqlite'));
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, "databases", "todo_hut_app.sqlite"),
+      filename: path.join(__dirname, 'databases', 'todo_hut_app.sqlite'),
     },
     migrations: {
-      directory: path.join(__dirname, ".", "app", "electron", "migrations"),
+      directory: path.join(__dirname, '.', 'app', 'electron', 'migrations'),
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: function (conn, done) {
+        conn.run("PRAGMA KEY = 'password'");
+        done();
+      },
+    },
   },
 
   staging: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, "databases", "todo_hut_app.sqlite"),
+      filename: path.join(__dirname, 'databases', 'todo_hut_app.sqlite'),
     },
     migrations: {
-      directory: path.join(__dirname, ".", "app", "electron", "migrations"),
+      directory: path.join(__dirname, '.', 'app', 'electron', 'migrations'),
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: function (conn, done) {
+        conn.run("PRAGMA KEY = 'password'");
+        done();
+      },
+    },
   },
 
   production: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, "databases", "todo_hut_app.sqlite"),
+      filename: path.join(__dirname, 'databases', 'todo_hut_app.sqlite'),
     },
     migrations: {
-      directory: path.join(__dirname, ".", "app", "electron", "migrations"),
+      directory: path.join(__dirname, '.', 'app', 'electron', 'migrations'),
     },
     useNullAsDefault: true,
+    pool: {
+      afterCreate: function (conn, done) {
+        conn.run("PRAGMA KEY = 'password'");
+        done();
+      },
+    },
   },
 };
